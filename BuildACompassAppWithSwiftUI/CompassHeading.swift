2ff7d8cfa821e81c40ffb18f7d2ae2aa05d6941d
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-14 14:50:40 +0300

    Compass logic

---
+++ BuildACompassAppWithSwiftUI/CompassHeading.swift
@@ -0,0 +1,43 @@
+//
+//  CompassHeading.swift
+//  BuildACompassAppWithSwiftUI
+//
+//  Created by ramil on 14.10.2019.
+//  Copyright © 2019 com.ri. All rights reserved.
+//
+
+import Foundation
+import Combine
+import CoreLocation
+
+class CompassHeading: NSObject, ObservableObject, CLLocationManagerDelegate {
+    var objectWillChange = PassthroughSubject<Void, Never>()
+    var degrees: Double = .zero {
+        didSet {
+            objectWillChange.send()
+        }
+    }
+    
+    private let locationManager: CLLocationManager
+    
+    override init() {
+        self.locationManager = CLLocationManager()
+        super.init()
+        
+        self.locationManager.delegate = self
+        self.setup()
+    }
+    
+    private func setup() {
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.headingAvailable() {
+            self.locationManager.startUpdatingLocation()
+            self.locationManager.startUpdatingHeading()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        self.degrees = -1 * newHeading.magneticHeading
+    }
+}
