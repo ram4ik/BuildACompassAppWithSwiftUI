11fe42c87963036c6240e0adf794fa0bc35c22ff
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-14 14:56:53 +0300

    update model hack

--- BuildACompassAppWithSwiftUI/ContentView.swift
+++ BuildACompassAppWithSwiftUI/ContentView.swift
@@ -36,25 +36,25 @@ struct Marker: Hashable {
         self.label = label
     }
 
+    func degreeText() -> String {
+        return String(format: "%.0f", self.degrees)
+    }
+
     static func markers() -> [Marker] {
         return [
-            Marker(degrees: 0, label: "N"),
+            Marker(degrees: 0, label: "S"),
             Marker(degrees: 30),
             Marker(degrees: 60),
-            Marker(degrees: 90, label: "E"),
+            Marker(degrees: 90, label: "W"),
             Marker(degrees: 120),
             Marker(degrees: 150),
-            Marker(degrees: 180, label: "S"),
+            Marker(degrees: 180, label: "N"),
             Marker(degrees: 210),
             Marker(degrees: 240),
-            Marker(degrees: 270, label: "w"),
+            Marker(degrees: 270, label: "E"),
             Marker(degrees: 300),
             Marker(degrees: 330)
         ]
-    }
-    
-    func degreeText() -> String {
-        return String(format: "%.0f", self.degrees)
     }
 }
 
